6673931819c428e028075feb83c13006f4349189
LiGM-GH <lipkin.grisha@mail.ru>
2022-06-23 07:52:14 +0300

    Created seeds file for simple data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+    title: "my_blog_#{blog}",
+    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '\
+    'Cras velit arcu, faucibus eget metus eu, eleifend volutpat purus. '\
+    'Maecenas at nulla sed mauris porttitor vulputate. '\
+    'Aenean et neque ut eros efficitur pulvinar at et massa. '\
+    'Fusce non aliquet lectus. '\
+    'In porttitor massa at dui ultricies pulvinar. Aenean id nunc ex. '
+  )
+end
+
+puts '10 blog posts created'
+
+5.times do |skill|
+  Skill.create!(
+    title: "Rails #{skill}",
+    percent_utilized: 15
+  )
+end
+
+puts '5 skills created'
+
+9.times do |portfolio_item|
+  Portfolio.create!(
+    title: "portfolio title #{portfolio_item}",
+    subtitle: "portfolio subtitle #{portfolio_item}",
+    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '\
+      'Cras velit arcu, faucibus eget metus eu, eleifend volutpat purus. '\
+      'Maecenas at nulla sed mauris porttitor vulputate. '\
+      'Aenean et neque ut eros efficitur pulvinar at et massa. '\
+      'Fusce non aliquet lectus. '\
+      'In porttitor massa at dui ultricies pulvinar. Aenean id nunc ex. ',
+    main_image: 'https://dummyimage.com/1040x740/fff/aaa',
+    thumb_image: 'https://dummyimage.com/640x360/fff/aaa'
+  )
+end
+
+puts '9 portfolio items created'
