1e55c98b705c108ab86bbde49f8924da67f87d70
LiGM-GH <lipkin.grisha@mail.ru>
2022-07-10 22:53:13 +0300

    Integrated new and created actions for the portfolios controller

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,18 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
   end
+
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    puts "Params[portfolio]: #{params[:portfolio]}"
+    @portfolio_item = Portfolio.new(params[:portfolio].permit(:title, :subtitle, :body))
+    if @portfolio_item.save
+      redirect_to portfolios_path, notice: 'Portfolio successfully created'
+    else
+      render 'new', notice: 'Something went wrong'
+    end
+  end
 end
