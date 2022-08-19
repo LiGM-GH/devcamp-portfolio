66cea357b81cc779ce6faf7b2713b0bd68050914
LiGM-GH <lipkin.grisha@mail.ru>
2022-08-19 13:22:00 +0300

    Integrated technologies model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -4,6 +4,7 @@ class Portfolio < ApplicationRecord
   include Placeholder
 
   validates_presence_of :title, :body, :main_image, :thumb_image
+  has_many :technologies
 
   scope :angular, -> { where subtitle: 'Angular' }
 
