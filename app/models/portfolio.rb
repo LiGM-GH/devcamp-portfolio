db42430395aaa337d765d6c5a9ed27f05472479a
LiGM-GH <lipkin.grisha@mail.ru>
2022-08-19 13:39:29 +0300

    Integrated nested attributes for Portfolio model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -5,6 +5,8 @@ class Portfolio < ApplicationRecord
 
   validates_presence_of :title, :body, :main_image, :thumb_image
   has_many :technologies
+  accepts_nested_attributes_for :technologies,
+                                reject_if: ->(attrs) { attrs['name'].blank? }
 
   scope :angular, -> { where subtitle: 'Angular' }
 
